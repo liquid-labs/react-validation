4ab765b9a424fd45ccf0d7f91e33c8aeed852132
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-12 15:37:41 -0500

    use vcAPI to get field value and test callback

--- js/utils/ValidationContext.test.js
+++ js/utils/ValidationContext.test.js
@@ -6,14 +6,14 @@ import { ValidationContext, useValidationContextAPI } from './ValidationContext'
 import { cleanup, fireEvent, render } from 'react-testing-library'
 import isEqual from 'lodash.isequal'
 
-const TestChild = ({fooValue}) => {
+const TestChild = () => {
   const vcAPI = useValidationContextAPI()
   return (
     <div>
       <input aria-label="foo" name="foo"
         onChange={(event) => vcAPI.updateFieldValue('foo', event.target.value)}
-        onBlur={(event) => vcAPI.blurField('foo')}
-        value={fooValue} />
+        onBlur={(event) => { console.log('blurring'); vcAPI.blurField('foo')}}
+        value={vcAPI.getFieldValue('foo')} />
       <span data-testid="isChanged">{vcAPI.isChanged() + ''}</span>
     </div>
   )
@@ -22,11 +22,13 @@ const TestChild = ({fooValue}) => {
 describe('ValidationContext', () => {
   const origData = { foo : 'foo', bar : 'bar' }
   let currData = Object.assign({}, origData)
-  const updateCallback = (newData) => currData = newData
+  const updateCallback = (newData) => {
+    currData = newData
+  }
 
   const { getByLabelText, getByTestId, rerender } = render(
     <ValidationContext data={currData} updateCallback={updateCallback}>
-      <TestChild fooValue={currData.foo} />
+      <TestChild/>
     </ValidationContext>
   )
 
@@ -42,5 +44,12 @@ describe('ValidationContext', () => {
 
   test('should not invoke callback until field blurred', () => {
     expect(isEqual(origData, currData)).toBe(true)
+  })
+
+  test('should invoke update callback on blur', () => {
+    const input = getByLabelText('foo')
+    fireEvent.blur(input)
+    expect(isEqual(origData, currData)).toBe(false)
+    expect(currData.foo).toBe('foo2')
   })
 })
