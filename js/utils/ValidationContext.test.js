611b02577d8e15cdc1d74bed7c0b90fdd059a384
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-12 15:18:39 -0500

    reworked testing to make sleeker and lighter

--- js/utils/ValidationContext.test.js
+++ js/utils/ValidationContext.test.js
@@ -4,36 +4,43 @@ import React from 'react'
 import { ValidationContext, useValidationContextAPI } from './ValidationContext'
 
 import { cleanup, fireEvent, render } from 'react-testing-library'
+import isEqual from 'lodash.isequal'
 
 const TestChild = ({fooValue}) => {
   const vcAPI = useValidationContextAPI()
   return (
     <div>
-      <input aria-label="foo" name="foo" onChange={(event) => vcAPI.updateFieldValue('foo', event.target.value)} value={fooValue} />
+      <input aria-label="foo" name="foo"
+        onChange={(event) => vcAPI.updateFieldValue('foo', event.target.value)}
+        onBlur={(event) => vcAPI.blurField('foo')}
+        value={fooValue} />
       <span data-testid="isChanged">{vcAPI.isChanged() + ''}</span>
     </div>
   )
 }
 
 describe('ValidationContext', () => {
-  afterEach(cleanup)
+  const origData = { foo : 'foo', bar : 'bar' }
+  let currData = Object.assign({}, origData)
+  const updateCallback = (newData) => currData = newData
+
+  const { getByLabelText, getByTestId, rerender } = render(
+    <ValidationContext data={currData} updateCallback={updateCallback}>
+      <TestChild fooValue={currData.foo} />
+    </ValidationContext>
+  )
+
+  test('should initially be unchanged', () => {
+    expect(getByTestId("isChanged").textContent).toBe('false')
+  })
 
   test('should recognize changed data', () => {
-    const data = { foo : 'foo', bar : 'bar' }
-    let currData = Object.assign({}, data)
-    const updateCallback = (newData) => currData = newData
-
-    const { getByLabelText, getByTestId, rerender } = render(
-      <ValidationContext data={data} updateCallback={updateCallback}>
-        <TestChild fooValue={currData.foo} />
-      </ValidationContext>
-    )
-
-    expect(getByTestId("isChanged").textContent).toBe('false')
-
     const input = getByLabelText('foo')
     fireEvent.change(input, { target : { value : 'foo2' } })
-
     expect(getByTestId("isChanged").textContent).toBe('true')
+  })
+
+  test('should not invoke callback until field blurred', () => {
+    expect(isEqual(origData, currData)).toBe(true)
   })
 })
