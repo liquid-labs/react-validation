29d1ae6ea204cc74daf5b3af7d065aee1b460159
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-12 17:04:57 -0500

    replace cool and fast sequental test with more standard 'do all the work every time approach; also, add tests for error message handling

--- js/utils/ValidationContext.test.jsx
+++ js/utils/ValidationContext.test.jsx
@@ -6,50 +6,84 @@ import { ValidationContext, useValidationContextAPI } from './ValidationContext'
 import { cleanup, fireEvent, render } from 'react-testing-library'
 import isEqual from 'lodash.isequal'
 
-const TestChild = () => {
+const TestChild = ({validators}) => {
   const vcAPI = useValidationContextAPI()
+  if (validators) {
+    vcAPI.updateFieldValidators('foo', validators)
+  }
   return (
     <div>
       <input aria-label="foo" name="foo"
         onChange={(event) => vcAPI.updateFieldValue('foo', event.target.value)}
-        onBlur={(event) => { console.log('blurring'); vcAPI.blurField('foo')}}
+        onBlur={(event) => vcAPI.blurField('foo')}
         value={vcAPI.getFieldInputValue('foo')} />
       <span data-testid="isChanged">{vcAPI.isChanged() + ''}</span>
+      <span data-testid="errorMsg">{vcAPI.getFieldErrorMessage('foo') + ''}</span>
     </div>
   )
 }
 
-describe('ValidationContext', () => {
-  const origData = { foo : 'foo', bar : 'bar' }
-  let currData = Object.assign({}, origData)
-  const updateCallback = (newData) => {
-    currData = newData
-  }
+const testValidators = [
+  (value) => value === '' ? "Required." : null,
+  (value) => value === 'bar' ? "No bar!" : null,
+]
 
-  const { getByLabelText, getByTestId, rerender } = render(
-    <ValidationContext data={currData} updateCallback={updateCallback}>
-      <TestChild/>
-    </ValidationContext>
-  )
+const stdSetup = (dataEnvelope, validators=undefined) => {
+  const origData = dataEnvelope.data || { foo : 'foo', bar : 'bar' }
+  dataEnvelope.data = Object.assign({}, origData)
+  const updateCallback = (newData) => dataEnvelope.data = newData
+
+  return {
+    ...render(
+      <ValidationContext data={dataEnvelope.data} updateCallback={updateCallback}>
+        <TestChild validators={validators}/>
+      </ValidationContext>
+    ),
+    origData,
+  }
+}
+
+describe('ValidationContext', () => {
+  afterEach(cleanup)
 
   test('should initially be unchanged', () => {
+    const { getByTestId } = stdSetup({})
     expect(getByTestId("isChanged").textContent).toBe('false')
   })
 
   test('should recognize changed data', () => {
+    const { getByLabelText, getByTestId } = stdSetup({})
     const input = getByLabelText('foo')
     fireEvent.change(input, { target : { value : 'foo2' } })
     expect(getByTestId("isChanged").textContent).toBe('true')
+    expect(input.value).toBe('foo2') // sanity check
   })
 
   test('should not invoke callback until field blurred', () => {
-    expect(isEqual(origData, currData)).toBe(true)
+    const dataEnvelope = {}
+    const { origData } = stdSetup(dataEnvelope)
+    expect(isEqual(origData, dataEnvelope.data)).toBe(true)
   })
 
   test('should invoke update callback on blur', () => {
+    const dataEnvelope = {}
+    const { origData, getByLabelText } = stdSetup(dataEnvelope)
+    const input = getByLabelText('foo')
+    fireEvent.change(input, { target : { value : 'foo2' } })
+    fireEvent.blur(input)
+    expect(isEqual(origData, dataEnvelope.data)).toBe(false)
+    expect(dataEnvelope.data.foo).toBe('foo2')
+  })
+
+  test('should not produce errors on invalid, untouched fields', () => {
+    const { getByTestId } = stdSetup({ data: { foo: null } }, testValidators)
+    expect(getByTestId("errorMsg").textContent).toBe('null')
+  })
+
+  test('should produce errors on invalid field after blur', () => {
+    const { getByLabelText, getByTestId } = stdSetup({ data : { foo: null } }, testValidators)
     const input = getByLabelText('foo')
     fireEvent.blur(input)
-    expect(isEqual(origData, currData)).toBe(false)
-    expect(currData.foo).toBe('foo2')
+    expect(getByTestId("errorMsg").textContent).toBe('Required.')
   })
 })
