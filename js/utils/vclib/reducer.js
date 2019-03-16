25cf5b7f443e6290b83d2d5681d99f5199b56b69
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-16 10:06:57 -0500

    fix context validation after field update

--- js/utils/vclib/reducer.js
+++ js/utils/vclib/reducer.js
@@ -62,11 +62,7 @@ const validateFieldValue = (value, validators) => {
 const validateContextValues = (fieldData, data, ...validatorInfosArr) => {
   validatorInfosArr.forEach((validatorInfos) => {
     if (validatorInfos && validatorInfos.length > 0) {
-      // wanted to spread the array in the forEach callback, but it was picking
-      // up the letters of the key rather than the string and function.
-      validatorInfos.forEach((vi) => {
-        const bindFieldName = vi[0]
-        const validator = vi[1]
+      validatorInfos.forEach(([bindFieldName, validator]) => {
         const fieldEntry = fieldData[bindFieldName] || { ...fieldEntryTemplate }
         fieldData[bindFieldName] = fieldEntry
         if (!fieldEntry.errorMsg) fieldEntry.errorMsg = validator(data)
@@ -78,7 +74,9 @@ const validateContextValues = (fieldData, data, ...validatorInfosArr) => {
 /**
  * 'exportDataFromState' reduces the 'state'
  */
-const exportDataFromState = (state) => Object.entries(state.fieldData)
+const exportDataFromState = (state) => exportDataFromFieldData(state.fieldData)
+
+const exportDataFromFieldData = (fieldData) => Object.entries(fieldData)
   .reduce((data, [fieldName, fieldEntry]) => {
     data[fieldName] = fieldEntry.value
     return data
@@ -189,7 +187,7 @@ const reducer = (state, action) => {
       fieldEntry.blurredAfterChange = false
 
       validateContextValues(newFieldData,
-        exportDataFromState(state),
+        exportDataFromFieldData(newFieldData),
         state.contextValidators[fieldName],
         state.contextValidators['*'])
 
@@ -205,9 +203,7 @@ const reducer = (state, action) => {
     const { fieldName } = action
     // This should not be possible outside of programatic flim-flammery.
     const fieldEntry = state.fieldData[fieldName] || { ...fieldEntryTemplate }
-    console.log('blurField state', state)
     if (!fieldEntry.touched || !fieldEntry.blurredAfterChange) {
-      console.log('doing it')
       const newHistory = processHistoryUpdate(state)
       return {
         ...state,
